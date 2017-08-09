a80a712c6a1ffb21128084c22bf0a2d19734f3a4
Peter Golm <golm.peter@gmail.com>
2017-08-09 17:09:11 +0200

    Add docker entrypoint

---
+++ 5.1/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+# if command starts with an option, prepend mysqld
+if [ "${1:0:1}" = '-' ]; then
+	set -- varnishd $@
+fi
+
+if [ "$1" = 'varnishd' ]; then
+	set $@ -F
+
+	if [ ! -e /etc/varnish/default.vcl ]; then
+		set $@ -f /etc/varnish/default.vcl
+	fi
+
+	if [ ! -e /etc/varnish/secret ]; then
+		set $@ -S /etc/varnish/secret
+	fi
+
+	if [ ! -z $VARNISH_ADMIN_LISTEN ]; then
+		set $@ -T $VARNISH_ADMIN_LISTEN
+	fi
+fi
+
+exec $@
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # About this Repo
 
+[![Travis](https://img.shields.io/travis/pgolm/docker-varnish.svg)](https://travis-ci.org/pgolm/docker-varnish)
+[![Docker Pulls](https://img.shields.io/docker/pulls/pgolm/varnish.svg)](https://hub.docker.com/r/pgolm/varnish/)
+[![Docker Automated buil](https://img.shields.io/docker/automated/pgolm/varnish.svg)](https://hub.docker.com/r/pgolm/varnish/)
+
 This is the git repo of the docker image of varnish.
